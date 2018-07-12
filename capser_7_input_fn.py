0550a01779c72495144be614511038ddcea5d13c
adriendoerig <adrien.doerig@gmail.com>
2018-07-12 15:45:37 +0200

    first tpu trial_h

--- capser_7_input_fn.py
+++ capser_7_input_fn.py
@@ -2,6 +2,68 @@ import numpy as np
 from parameters import *
 from tensorflow.contrib.data import batch_and_drop_remainder
 
+def parse_multi_shape(serialized):
+    # variables are in byte form in the tfrecord file. this converts back to the right format.
+    # Define a dict with the data-names and types we expect to
+    # find in the TFRecords file.
+    # It is a bit awkward that this needs to be specified again,
+    # because it could have been written in the header of the
+    # TFRecords file instead.
+    features = \
+        {
+            'multi_shape_img': tf.FixedLenFeature([], tf.string),
+            'single_shape_img': tf.FixedLenFeature([], tf.string),
+            'labels': tf.FixedLenFeature([], tf.string),
+            'vernier_label': tf.FixedLenFeature([], tf.string),
+            'n_elements': tf.FixedLenFeature([], tf.string)
+        }
+
+    # Parse the serialized data so we get a dict with our data.
+    parsed_example = tf.parse_single_example(serialized=serialized, features=features)
+
+    # Get the image as raw bytes.
+    multi_shape_img_raw = parsed_example['multi_shape_img']
+    single_shape_img_raw = parsed_example['single_shape_img']
+    labels_raw = parsed_example['labels']
+    vernier_label_raw = parsed_example['vernier_label']
+    n_elements_raw = parsed_example['n_elements']
+
+    # Decode the raw bytes so it becomes a tensor with type.
+    multi_shape_img = tf.decode_raw(multi_shape_img_raw, tf.float32)
+    single_shape_img = tf.decode_raw(single_shape_img_raw, tf.float32)
+    labels = tf.decode_raw(labels_raw, tf.float32)
+    vernier_label = tf.decode_raw(vernier_label_raw, tf.int64)
+    n_elements = tf.decode_raw(n_elements_raw, tf.int64)
+
+    return multi_shape_img, single_shape_img, labels, vernier_label, n_elements
+
+
+def parse_config(serialized):
+    # variables are in byte form in the tfrecord file. this converts back to the right format.
+    # Define a dict with the data-names and types we expect to
+    # find in the TFRecords file.
+    # It is a bit awkward that this needs to be specified again,
+    # because it could have been written in the header of the
+    # TFRecords file instead.
+    features = \
+        {
+            'config_img': tf.FixedLenFeature([], tf.string),
+            'vernier_label': tf.FixedLenFeature([], tf.string),
+        }
+
+    # Parse the serialized data so we get a dict with our data.
+    parsed_example = tf.parse_single_example(serialized=serialized, features=features)
+
+    # Get the image as raw bytes.
+    config_img_raw = parsed_example['config_img']
+    vernier_label_raw = parsed_example['vernier_label']
+
+    # Decode the raw bytes so it becomes a tensor with type.
+    config_img = tf.decode_raw(config_img_raw, tf.float32)
+    vernier_label = tf.decode_raw(vernier_label_raw, tf.float32)
+
+    return config_img, vernier_label
+
 def input_fn_multi_shape(filenames, train, n_epochs=n_epochs, batch_size=batch_size, buffer_size=buffer_size):
     # needed for estimators in the main script
     # Args:
