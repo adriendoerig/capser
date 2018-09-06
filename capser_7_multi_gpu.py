bfe1b3a6cf424052b0bd777aabe1584fd8651fa5
adriendoerig <adrien.doerig@gmail.com>
2018-09-06 15:53:41 +0200

    06092018b

--- capser_7_multi_gpu.py
+++ capser_7_multi_gpu.py
@@ -14,6 +14,14 @@ print('#################### MODEL_NAME_VERSION: ' + MODEL_NAME + ' #############
 # create estimator for model (the model is described in capser_7_model_fn)
 capser = tf.estimator.Estimator(model_fn=model_fn_temp, params={'model_batch_size': batch_size}, model_dir=checkpoint_path)
 
+# train model
+logging.getLogger().setLevel(logging.INFO)  # to show info about training progress
+
+train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=n_steps)
+eval_spec = tf.estimator.EvalSpec(lambda: input_fn_config(data_path+'/test_squares.tfrecords'), steps=100)
+
+tf.estimator.train_and_evaluate(capser, train_spec, eval_spec)
+
 logging.getLogger().setLevel(logging.CRITICAL)  # to show less info in the console
 
 n_expt_batches = 50
