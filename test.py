208d50cc20ebe1c7f71f7c0b8c83bd486e8ec482
LynnSchmittwilken <43323675+LynnSchmittwilken@users.noreply.github.com>
2018-09-27 11:17:44 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from capsule_functions import safe_norm
-from batchMaker import StimMaker
-import tensorflow.contrib.framework
-
