d4a82d04a07c06abd64cae6a4aaf9d0fe82ce176
lynnschmittwilken <l.schmittwilken@web.de>
2018-09-28 13:35:00 +0200

    this time I really updated batchmaker and parameters to the new dataset

--- parameters.py
+++ parameters.py
@@ -11,23 +11,23 @@ create_new_train_set = False                         # if you already have a tfR
 train_data_path = data_path+'/train.tfrecords'      # where the training data file is located
 test_data_path = data_path+'/test_squares.tfrecords'
 n_train_samples = 500000                            # number of different stimuli in an epoch
-batch_size = 64 #random.randint(4,16) * 4                                    # stimuli per batch
+batch_size = 16 #random.randint(4,16) * 4                                    # stimuli per batch
 batch_size_per_shard = int(batch_size/1)                 # there are 8 shards on the TPU, each takes care of 1/8th of a batch
 buffer_size = 1024#1*1024*1024                           # number of stimuli simultaneously in memory (I think). Value taken from the tf TPU help page
 n_epochs = 1                                        # number of epochs
-number_of_steps = 75000#n_train_samples*n_epochs/batch_size       # number of training steps
+n_steps = 75000#n_train_samples*n_epochs/batch_size       # number of training steps
 check_data = None                                   # specify the path to a dataset you would like to look at. use None if you don't want to check any.
 
 # testing sets
 create_new_test_sets = False                        # if you already have tfRecords testing files in data_path, you may set to False
-n_test_samples = 64                                    # number of stimuli for each testing condition
+n_test_samples = 200                                    # number of stimuli for each testing condition
 test_stimuli = {'squares':       [None, [[1]], [[1, 1, 1, 1, 1]]],
                 'circles':       [None, [[2]], [[2, 2, 2, 2, 2]]],
                 'hexagons':      [None, [[3]], [[3, 3, 3, 3, 3]]],
                 'octagons':      [None, [[4]], [[4, 4, 4, 4, 4]]],
                 '4stars':        [None, [[5]], [[5, 5, 5, 5, 5]]],
                 '7stars':        [None, [[6]], [[6, 6, 6, 6, 6]]],
-                'squares_stars': [None, [[1]], [[1, 6, 1, 6, 1]]]}
+                'squares_stars': [None, [[1]], [[6, 1, 6, 1, 6]]]}
 # test_stimuli = {'squares':       [None, [[1]], [[1, 1, 1, 1, 1, 1, 1]]],
 #                 'circles':       [None, [[2]], [[2, 2, 2, 2, 2, 2, 2]]],
 #                 'hexagons':      [None, [[3]], [[3, 3, 3, 3, 3, 3, 3]]],
@@ -46,8 +46,8 @@ normalize_sets = False          # compute mean and std over 100 images and use t
 max_rows, max_cols = 1, 5       # max number of rows, columns of shape grids
 vernier_grids = False           # if true, verniers come in grids like other shapes. Only single verniers otherwise.
 vernier_normalization_exp = 0   # to give more importance to the vernier (see batchMaker). Use 0 for no effect. > 0  -> favour vernier during training
-im_size = (45, 100)             # IF USING THE DECONVOLUTION DECODER NEED TO BE EVEN NUMBERS (NB. this suddenly changed. before that, odd was needed... that's odd.)
-shape_size = 15                 # size of a single shape in pixels
+im_size = (65, 120)             # IF USING THE DECONVOLUTION DECODER NEED TO BE EVEN NUMBERS (NB. this suddenly changed. before that, odd was needed... that's odd.)
+shape_size = 17                 # size of a single shape in pixels
 random_size = True              # shape_size will vary around shape_size
 test_random_size = False        # same for test set
 random_pixels = .5              # stimulus pixels are drawn from random.uniform(1-random_pixels,1+random_pixels). So use 0 for deterministic stimuli. see batchMaker.py
@@ -81,13 +81,13 @@ if conv_batch_norm:
 else:
     conv_activation_function = tf.nn.elu
 conv1_params = {"filters": 32,
-                "kernel_size": 6,
+                "kernel_size": 7,
                 "strides": 1,
                 "padding": "valid",
                 "activation": conv_activation_function,
                 }
 conv2_params = {"filters": 32,
-                "kernel_size": 6,
+                "kernel_size": 7,
                 "strides": 2,
                 "padding": "valid",
                 "activation": conv_activation_function,
@@ -177,15 +177,7 @@ if normalize_images is True:
     normalization_text += '_VERNIERNORMEXP_'+str(vernier_normalization_exp)
 if normalize_sets is True:
     normalization_text += '_NORMSETS'
-deconv_decoder_text = ''
-if output_decoder_deconv_params['use_deconvolution_decoder']:
-    deconv_decoder_text = '_DECONVDECODER_'+str(output_decoder_deconv_params['use_deconvolution_decoder'])
-bn_text=''
-if conv_batch_norm:
-    bn_text += '_CONVBN_'+str(conv_batch_norm)
-if decoder_batch_norm:
-    bn_text += '_DECODERBN_'+str(decoder_batch_norm)
-MODEL_NAME = 'BS_'+str(batch_size)+'_CONV1KER_'+str(conv1_params['kernel_size'])+'_CONV2KER_'+str(conv2_params['kernel_size'])+'_CONVCAPSKER_'+str(conv_caps_params['kernel_size'])+'_C1DIM_'+str(caps1_n_dims)+'_C2DIM_'+str(caps2_n_dims)+'_LR_'+str(learning_rate)+bn_text+deconv_decoder_text+normalization_text
+MODEL_NAME = 'BS_'+str(batch_size)+'_C1DIM_'+str(caps1_n_dims)+'_C2DIM_'+str(caps2_n_dims)+'_LR_'+str(learning_rate)+'_CONVBN_'+str(conv_batch_norm)+'_DECODERBN_'+str(decoder_batch_norm)+'_DECONVDECODER_'+str(output_decoder_deconv_params['use_deconvolution_decoder'])+normalization_text
 LOGDIR = data_path + '/LOGDIR/' + MODEL_NAME + '/'  # will be redefined below
 # if not os.path.exists(LOGDIR):
 #     os.makedirs(LOGDIR)
@@ -221,6 +213,7 @@ def save_params(variables=locals()):
                      and ('function' not in str(value) and ('TextIOWrapper' not in str(value)))}
         [f.write(str(key) + ' : ' + str(value) + '\n') for key, value in variables.items()]
         print('Parameter values saved.')
+save_params()
 
 
 ### choose from optional plots, etc ###
